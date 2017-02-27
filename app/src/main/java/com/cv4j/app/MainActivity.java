747d8a1111332c0a2cd09d5e8a3fcb48f576d63c
fengzhizi715 <fengzhizi715@126.com>
2017-02-27 11:21:59 +0800

    add demo app

--- app/src/main/java/com/cv4j/app/MainActivity.java
+++ app/src/main/java/com/cv4j/app/MainActivity.java
@@ -1,7 +1,9 @@
-package com.cv4j;
+package com.cv4j.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.cv4j.R;
 
 public class MainActivity extends AppCompatActivity {
 
