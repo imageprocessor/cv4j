bc21f222a539bb049e78c1ebda89b365e9ac4466
fengzhizi715 <fengzhizi715@126.com>
2017-03-15 22:16:43 +0800

    完善demo

--- app/src/main/java/com/cv4j/app/activity/UseFilterWithRxActivity.java
+++ app/src/main/java/com/cv4j/app/activity/UseFilterWithRxActivity.java
@@ -14,6 +14,8 @@ import com.cv4j.rxjava.RxImageData;
 import com.safframework.injectview.annotations.InjectView;
 
 import io.reactivex.functions.Consumer;
+import thereisnospon.codeview.CodeView;
+import thereisnospon.codeview.CodeViewTheme;
 
 /**
  * Created by Tony Shen on 2017/3/13.
@@ -23,6 +25,9 @@ public class UseFilterWithRxActivity extends BaseActivity {
 
     @InjectView(R.id.image)
     ImageView image;
+
+    @InjectView(R.id.codeview)
+    CodeView codeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,5 +54,23 @@ public class UseFilterWithRxActivity extends BaseActivity {
                 image.setImageBitmap(imageData.toBitmap());
             }
         });
+
+        codeView.setTheme(CodeViewTheme.ANDROIDSTUDIO).fillColor();
+
+        StringBuilder code = new StringBuilder();
+        code.append("RxImageData.imageData(bitmap)")
+                .append("\r\n")
+                .append("    .addFilter(new NatureFilter())")
+                .append("\r\n")
+                .append("    .toFlowable()").append("\r\n")
+                .append("    .compose(RxImageData.toMain())").append("\r\n")
+                .append("    .subscribe(new Consumer<ImageData>() {").append("\r\n").append("\r\n")
+                .append("  @Override").append("\r\n")
+                .append("  public void accept(ImageData imageData) throws Exception {").append("\r\n")
+                .append("     image.setImageBitmap(imageData.toBitmap());").append("\r\n")
+                .append("  }").append("\r\n")
+                .append("});");
+
+        codeView.showCode(code.toString());
     }
 }
