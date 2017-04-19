1e9f7347c1074b0bda92819361936efc3ae727c3
zhigang <zhigang@2012-20160406GM>
2017-04-19 16:58:32 +0800

    add new data model

---
+++ cv4j/src/main/java/com/cv4j/core/datamodel/Vec3i.java
@@ -0,0 +1,17 @@
+package com.cv4j.core.datamodel;
+
+/**
+ * Created by Administrator on 2017/4/19.
+ */
+
+public class Vec3i {
+    public int x;
+    public int y;
+    public int z;
+
+    public Vec3i() {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+}
