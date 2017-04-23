5e0d98ddf63ac5571d8bf2589f5be40c03808e5f
fengzhizi715 <fengzhizi715@126.com>
2017-04-23 16:33:15 +0800

    fix GlowFilter issue

--- cv4j/src/main/java/com/cv4j/core/filters/GlowFilter.java
+++ cv4j/src/main/java/com/cv4j/core/filters/GlowFilter.java
@@ -3,7 +3,6 @@
  */
 package com.cv4j.core.filters;
 
-import com.cv4j.core.datamodel.ColorProcessor;
 import com.cv4j.core.datamodel.ImageProcessor;
 import com.cv4j.image.util.Tools;
 
@@ -21,43 +20,35 @@ public class GlowFilter extends GaussianBlurFilter{
 	}
 
 	@Override
-	public ImageProcessor filter(ImageProcessor src){
-
-		if (!(src instanceof ColorProcessor)) return src;
-
-		int width = src.getWidth();
-		int height = src.getHeight();
+	public ImageProcessor doFilter(ImageProcessor src){
 
 		int total = width*height;
-		byte[] R1 = new byte[total];((ColorProcessor)src).getRed();
-		byte[] G1 = new byte[total]; ((ColorProcessor)src).getGreen();
-		byte[] B1 = new byte[total]; ((ColorProcessor)src).getBlue();
-		System.arraycopy(((ColorProcessor)src).getRed(), 0, R1, 0, total);
-		System.arraycopy(((ColorProcessor)src).getGreen(), 0, G1, 0, total);
-		System.arraycopy(((ColorProcessor)src).getBlue(), 0, B1, 0, total);
+		byte[] R1 = new byte[total];
+		byte[] G1 = new byte[total];
+		byte[] B1 = new byte[total];
+		System.arraycopy(R, 0, R1, 0, total);
+		System.arraycopy(G, 0, G1, 0, total);
+		System.arraycopy(B, 0, B1, 0, total);
 
 		// 高斯模糊
-		ImageProcessor blurImage = super.filter(src);
-		byte[] R2 = new byte[total];((ColorProcessor)blurImage).getRed();
-		byte[] G2 = new byte[total]; ((ColorProcessor)blurImage).getGreen();
-		byte[] B2 = new byte[total]; ((ColorProcessor)blurImage).getBlue();
+		super.doFilter(src);
 
 		float a = 4*amount;
 
 		int index = 0;
 		for ( int y = 0; y < height; y++ ) {
 			for ( int x = 0; x < width; x++ ) {
-				int r1 = R2[index] & 0xff;
-				int g1 = G2[index] & 0xff;
-				int b1 = B2[index] & 0xff;
+				int r1 = R[index] & 0xff;
+				int g1 = G[index] & 0xff;
+				int b1 = B[index] & 0xff;
 
 				int r2 = R1[index] & 0xff;
 				int g2 = G1[index] & 0xff;
 				int b2 = B1[index] & 0xff;
 
-				R2[index] = (byte)Tools.clamp( (int)(r1 + a * r2) );
-				G2[index] = (byte)Tools.clamp( (int)(g1 + a * g2) );
-				B2[index] = (byte)Tools.clamp( (int)(b1 + a * b2) );
+				R[index] = (byte)Tools.clamp( (int)(r1 + a * r2) );
+				G[index] = (byte)Tools.clamp( (int)(g1 + a * g2) );
+				B[index] = (byte)Tools.clamp( (int)(b1 + a * b2) );
 				index++;
 			}
 		}
