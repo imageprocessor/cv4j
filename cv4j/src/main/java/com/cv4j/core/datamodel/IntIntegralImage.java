2a14948cd7a0f2fc464764e872ecf1e844eeaa83
zhigang <zhigang@2012-20160406GM>
2017-04-23 16:42:36 +0800

    skin smooth and make it beautyful

--- cv4j/src/main/java/com/cv4j/core/datamodel/IntIntegralImage.java
+++ cv4j/src/main/java/com/cv4j/core/datamodel/IntIntegralImage.java
@@ -4,6 +4,7 @@ public class IntIntegralImage {
 	// sum index tables
 	private int[] sum;
 	// image
+	private float[] squaresum;
 	private byte[] image;
 	private int width;
 	private int height;
@@ -41,6 +42,31 @@ public class IntIntegralImage {
 		return ((sum1 + sum4) - sum2 - sum3);
 	}
 
+	public float getBlockSquareSum(int x, int y, int m, int n) {
+		int swx = x + n/2;
+		int swy = y + m/2;
+		int nex = x-n/2-1;
+		int ney = y-m/2-1;
+		float sum1, sum2, sum3, sum4;
+		if(swx >= width) {
+			swx = width - 1;
+		}
+		if(swy >= height) {
+			swy = height - 1;
+		}
+		if(nex < 0) {
+			nex = 0;
+		}
+		if(ney < 0) {
+			ney = 0;
+		}
+		sum1 = squaresum[ney*width+nex];
+		sum4 = squaresum[swy*width+swx];
+		sum2 = squaresum[swy*width+nex];
+		sum3 = squaresum[ney*width+swx];
+		return ((sum1 + sum4) - sum2 - sum3);
+	}
+
 	public void process(int width, int height) {
 		this.width = width;
 		this.height = height;
@@ -60,6 +86,37 @@ public class IntIntegralImage {
 				p3=(uprow<0) ? 0:sum[offset-width]; // p(x, y-1);
 				p4=(uprow<0||leftcol<0) ? 0:sum[offset-width-1]; // p(x-1, y-1);
 				sum[offset]= p1+p2+p3-p4;
+				offset++;
+			}
+		}
+	}
+
+	public void process(int width, int height, boolean includeSqrt) {
+		this.width = width;
+		this.height = height;
+		sum = new int[width*height];
+		squaresum = new float[width*height];
+		// rows
+		int p1=0, p2=0, p3=0, p4;
+		int offset = 0, uprow=0, leftcol=0;
+		float sp2=0, sp3=0, sp4=0;
+		for(int row=0; row<height; row++ ) {
+			offset = row*width;
+			uprow = row-1;
+			for(int col=0; col<width; col++) {
+				leftcol=col-1;
+				// 计算和查找表
+				p1=image[offset]&0xff;// p(x, y)
+				p2=(leftcol<0) ? 0:sum[offset-1]; // p(x-1, y)
+				p3=(uprow<0) ? 0:sum[offset-width]; // p(x, y-1);
+				p4=(uprow<0||leftcol<0) ? 0:sum[offset-width-1]; // p(x-1, y-1);
+				sum[offset]= p1+p2+p3-p4;
+
+				// 计算平方查找表
+				sp2=(leftcol<0) ? 0:squaresum[offset-1]; // p(x-1, y)
+				sp3=(uprow<0) ? 0:squaresum[offset-width]; // p(x, y-1);
+				sp4=(uprow<0||leftcol<0) ? 0:squaresum[offset-width-1]; // p(x-1, y-1);
+				squaresum[offset]=p1*p1+sp2+sp3-sp4;
 				offset++;
 			}
 			// System.out.println();
---
+++ cv4j/src/main/java/com/cv4j/core/filters/BeautySkinFilter.java
@@ -0,0 +1,78 @@
+package com.cv4j.core.filters;
+
+import com.cv4j.core.datamodel.ColorProcessor;
+import com.cv4j.core.datamodel.ImageProcessor;
+import com.cv4j.core.datamodel.IntIntegralImage;
+import com.cv4j.image.util.Tools;
+
+/**
+ * good method try to smooth the minor noise
+ * @author zhigang jia
+ * @date 2017-04-23
+ *
+ */
+public class BeautySkinFilter implements CommonFilter {
+
+	private int xr;
+	private int yr;
+	private float sigma;
+	public BeautySkinFilter() {
+		sigma = 30.0f; // by default
+		xr = 5;
+		yr = 5;
+	}
+	
+	public void setWinsize(int radius) {
+		this.xr = radius;
+		this.yr = radius;
+	}
+
+	public float getSigma() {
+		return sigma;
+	}
+
+	public void setSigma(float sigma) {
+		this.sigma = sigma;
+	}
+
+	@Override
+	public ImageProcessor filter(ImageProcessor src) {
+		// get image data
+		int width = src.getWidth();
+		int height = src.getHeight();
+
+		// start ep process
+		byte[] output = new byte[width*height];
+		IntIntegralImage ii = new IntIntegralImage();
+		for(int i=0; i<src.getChannels(); i++) {
+			ii.setImage(src.toByte(i));
+			ii.process(width, height, true);
+			processSingleChannel(width, height, ii, output);
+			System.arraycopy(output, 0, src.toByte(i), 0, output.length);
+		}
+		// release memory
+		output = null;
+		return src;
+	}
+
+	public void processSingleChannel(int width, int height, IntIntegralImage input, byte[] output) {
+		float sigma2 = sigma*sigma;
+		int offset = 0;
+		int wy = (yr * 2 + 1);
+		int wx = (xr * 2 + 1);
+		int size = wx * wy;
+		int r = 0;
+		for (int row = 0; row < height; row++) {
+			offset = row * width;
+			for (int col = 0; col < width; col++) {
+				int sr = input.getBlockSum(col, row, wy, wx);
+				float a = input.getBlockSquareSum(col, row, wy, wx);
+				float b = sr / size;
+				float c = (a - (sr*sr)/size)/size;
+				float d = c / (c+sigma2);
+				r = (int)((1-d)*b + d*r);
+				output[offset + col] = (byte)Tools.clamp(r);
+			}
+		}
+	}
+}
