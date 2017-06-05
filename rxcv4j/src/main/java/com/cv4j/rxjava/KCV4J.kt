da698f904dde98a1c575bf3cdfbb87a5353d8967
fengzhizi715 <fengzhizi715@126.com>
2017-06-05 23:44:38 +0800

    add code comment

--- rxcv4j/src/main/java/com/cv4j/rxjava/KCV4J.kt
+++ rxcv4j/src/main/java/com/cv4j/rxjava/KCV4J.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2017-present, CV4J Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cv4j.rxjava
 
 import android.graphics.Bitmap
@@ -5,7 +20,7 @@ import android.widget.ImageView
 import com.cv4j.core.filters.CommonFilter
 
 /**
- * Created by Tony Shen on 2017/6/5.
+ * only for Kotlin code,this class provides the DSL style for cv4j
  */
 class Wrapper {
 
