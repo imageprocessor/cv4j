aed892b9dd42287348257d69d0e09e2d45063bc2
fengzhizi715 <fengzhizi715@126.com>
2017-07-08 20:02:48 +0800

    update ImageCodes

--- cv4j/src/main/java/com/cv4j/image/util/ImageCodecs.java
+++ cv4j/src/main/java/com/cv4j/image/util/ImageCodecs.java
@@ -1,46 +1,62 @@
+/*
+ * Copyright (c) 2017-present, CV4J Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cv4j.image.util;
 
 import com.cv4j.core.datamodel.CV4JImage;
+import com.cv4j.exception.CV4JException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import ar.com.hjg.pngj.ImageInfo;
+import ar.com.hjg.pngj.ImageLineInt;
+import ar.com.hjg.pngj.PngReader;
 import edu.uthscsa.ric.volume.formats.jpeg.JPEGLosslessDecoderWrapper;
-
-/**
- * Created by gloomy fish on 2017/7/8.
- */
 
 public class ImageCodecs {
 
     public static CV4JImage read(String filePath) {
         CV4JImage image = null;
+        File file = new File(filePath);
+
         if (filePath.endsWith(".jpg") || filePath.endsWith(".JPG")
                 || filePath.endsWith(".JPEG") || filePath.endsWith(".jpeg")) {
             try {
-                image = JPEGLosslessDecoderWrapper.readImage(getBytesFromFile(new File(filePath)));
+                image = JPEGLosslessDecoderWrapper.readImage(getBytesFromFile(file));
             } catch (IOException ioe) {
 
             }
         } else if (filePath.endsWith(".png") || filePath.endsWith(".PNG")) {
-            // TODO: zhigang
-            PngReader pngr = new PngReader(new File(filePath));
-            System.out.println(pngr.toString());
+            PngReader pngr = new PngReader(file);
             int channels = pngr.imgInfo.channels;
             if (channels < 3 || pngr.imgInfo.bitDepth != 8)
-                throw new RuntimeException("This method is for RGB8/RGBA8 images");
+                throw new CV4JException("This method is for RGB8/RGBA8 images");
 
-            ImageInfo info = pngr.getImgInfo();
+            ImageInfo info = pngr.imgInfo;
             int width = info.cols;
             int height = info.rows;
             int dims = info.channels;
             int r = 0, g = 0, b = 0;
             int index = 0;
             int[] pixels = new int[width * height];
+            ImageLineInt line = null;
             for (int row = 0; row < height; row++) {
-                ImageLineInt line = (ImageLineInt) pngr.readRow(row);
+                line = (ImageLineInt) pngr.readRow(row);
                 int[] data = line.getScanline();
                 for (int col = 0; col < width; col++) {
                     if (dims == 3) {
@@ -60,6 +76,7 @@ public class ImageCodecs {
             }
             image = new CV4JImage(width, height, pixels);
         }
+
         return image;
     }
 
