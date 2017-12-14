88b221ea6c0fe5c9b75fa96e226326088cf0cd82
gloomyfish1998 <57558865@qq.com>
2017-12-14 09:13:45 +0800

    bug:fast rotate issue

--- cv4j/src/main/java/com/cv4j/core/pixels/FastRotate.java
+++ cv4j/src/main/java/com/cv4j/core/pixels/FastRotate.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2017-present, CV4J Contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.cv4j.core.pixels;
 
 import com.cv4j.core.datamodel.ByteProcessor;
@@ -33,6 +18,18 @@ public class FastRotate {
 	}
 	
 	public ImageProcessor rotate(ImageProcessor processor, double degree, Scalar bgColor) {
+		this.color = bgColor;
+        if(Math.floor(degree/90) == 1){
+        	processor = rotate90(processor, 90);
+        	degree = degree - 90;
+        } else if(Math.floor(degree/90) == 2) {
+        	processor = rotate90(processor, 180);
+        	degree = degree - 180;
+        }
+        else if(Math.floor(degree/90) == 3) {
+        	processor = rotate90(processor, 270);
+        	degree = degree - 270;
+        }
 		int width = processor.getWidth();
 		int height = processor.getHeight();
 		int srcHeight = processor.getHeight();
@@ -108,6 +105,56 @@ public class FastRotate {
         }
         return temp;
 	}
+	
+	private ImageProcessor rotate90(ImageProcessor processor, float degree) {
+		int width = processor.getWidth();
+		int height = processor.getHeight();
+		int outw = -1;
+		int outh = -1;
+		int ch = processor.getChannels();
+        int index = 0;
+        int index2 = 0;
+		if(degree == 90)
+		{
+			outw = height;
+			outh = width;
+		}
+		else if(degree == 180)
+		{
+			outw = width;
+			outh = height;
+		}
+		else if(degree == 270)
+		{
+			outw = height;
+			outh = width;
+		}
+		ImageProcessor dst = (ch == 3) ? new ColorProcessor(outw, outh) : new ByteProcessor(outw, outh);
+
+        for(int row=0; row<height; row++) {
+        	for(int col=0; col<width; col++) {
+        		index = row * width + col;
+        		if(degree == 90)
+        		{
+        			index2 = outw * col + (height - 1- row);
+        		}
+        		else if(degree == 180)
+        		{
+        			index2 = outw * (height - 1 - row) + 
+        					(width - 1 - col);
+        		}
+        		else if(degree == 270)
+        		{
+        			index2 = outw * (width - 1 - col) + row;
+        		}
+        		for(int i=0; i<ch; i++) {
+        			dst.toByte(i)[index2] = processor.toByte(i)[index];
+        		}
+        	}
+        }
+        return dst;
+	}
+
 	
 	private byte[] getNearestPixels(ImageProcessor processor, int width, int height, 
 			double prow, double pcol, boolean yshear) {
